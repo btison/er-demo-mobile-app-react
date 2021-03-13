d84ee1379b2d1aba6b998eb903f52b328bd3a5c1
Bernard Tison <bernard.tison@gmail.com>
2021-03-13 22:18:39 +0100

    Kafka connection  and consume messages from topic-mission-event

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import path from 'path';
 import { createProxyMiddleware } from 'http-proxy-middleware';
+import { Kafka, logLevel } from 'kafkajs';
 
 const app = express();
 
@@ -60,6 +61,41 @@ app.use(
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
 });
+
+// setup kafka connection
+const kafka = new Kafka({
+    logLevel: logLevel.INFO,
+    brokers: app.get('kafka-host'),
+    connectionTimeout: 3000
+});
+const consumer = kafka.consumer({ groupId: app.get('kafka-groupid') });
+
+const run = async () => {
+    console.log('Setting up Kafka client for ', app.get('kafka-host'));
+    await consumer.connect();
+
+    app.get('kafka-message-topic').forEach((t: string) => {
+        const run2 = async () => {
+            console.log('Setting up Kafka client for ', app.get('kafka-host'), 'on topic', t);
+            await consumer.subscribe({ topic: t });
+        }
+        run2().catch(e => console.error(`[server.js] ${e.message}`, e))
+    });
+
+    await consumer.run({
+        eachMessage: async ({ topic, partition, message }) => {
+            try {
+                console.log(message);
+            } catch (err) {
+                console.error(`Error when transforming incoming message to CloudEvent. ${err.message}`, err);
+                console.error('    Topic: ', topic);
+                console.error('    Message:', message);
+            }
+        },
+    })
+};
+
+run().catch(e => console.error(`[server.js] ${e.message}`, e))
 
 // start express server on port 8080
 app.listen(app.get('port'), () => {
